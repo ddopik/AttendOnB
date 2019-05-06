02020c2dab4c202e9d8cc438c8dd776c4cd498ea
softmills.developer@gmail.com <softmills.developer@gmail.com>
2019-05-06 15:13:23 +0200

    saving state

--- app/src/main/java/com/example/attendonb/ui/LoginActivity.kt
+++ app/src/main/java/com/example/attendonb/ui/LoginActivity.kt
@@ -4,30 +4,29 @@ import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.Observer
 import com.example.attendonb.R
- import androidx.fragment.app.FragmentActivity
 import com.example.attendonb.base.BaseFragmentActivity
 import com.example.attendonb.ui.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseFragmentActivity() {
 
-    var loginViewModel:LoginViewModel ?=null
+    var loginViewModel: LoginViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        loginViewModel=LoginViewModel.getInstance(this)
+        loginViewModel = LoginViewModel.getInstance(this)
 
-     }
+    }
 
     override fun initObservers() {
         loginViewModel?.isDataLoading?.observe(this, Observer {
 
-            if (it){
-                login_progress.visibility= View.VISIBLE
-            }else{
-                login_progress.visibility= View.GONE
+            if (it) {
+                login_progress.visibility = View.VISIBLE
+            } else {
+                login_progress.visibility = View.GONE
             }
         })
 
